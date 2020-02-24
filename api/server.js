0650865383a9cf276f1088d8e40e33c27246f045
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-24 15:00:46 -0500

    setup sessions in server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
@@ -8,9 +9,29 @@ const knex = require("../data/db-config.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "login_cookie",
+  secret: process.env.SECRET,
+  saveUninitialized: true,
+  resave: false,
+  store: new KnexSessionsStore({
+    knex,
+    createTable: true,
+    clearInterval: 1000 * 60 * 10,
+    sidfieldname: "sid",
+    tablename: "sessions"
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    httpOnly: true,
+    secure: false
+  }
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(sessions(sessionConfig));
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "is live!" });
