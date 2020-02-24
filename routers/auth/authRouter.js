b0f455b2e8fe4bfc6a3c38eac4c261834c37ca9f
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-24 15:36:08 -0500

    added login endpoint and setup cookies upon login

--- routers/auth/authRouter.js
+++ routers/auth/authRouter.js
@@ -30,4 +30,27 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const userData = req.body;
+
+  if (userData.username && userData.password) {
+    try {
+      const user = await Auth.getBy({ username: userData.username });
+
+      if (user && bc.compareSync(userData.password, user.password)) {
+        //   provide user cookie after login
+        req.session.user = user;
+
+        res.status(200).json({ message: "Logged In!" });
+      } else {
+        res.status(401).json({ error: "You shall not pass" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: "User does not exist in database" });
+    }
+  } else {
+    res.status(400).json({ error: "Please provide credentials" });
+  }
+});
+
 module.exports = router;
