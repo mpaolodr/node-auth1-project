25ec250813b0fb0c1e7b27fb400ffec7a9725247
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-24 15:57:18 -0500

    refactored middleware to use cookies

--- api/middlewares/restricted-middleware.js
+++ api/middlewares/restricted-middleware.js
@@ -4,24 +4,31 @@ const Auth = require("../../routers/auth/auth-model.js");
 
 async function restricted(req, res, next) {
   // without cookies
-  const { username, password } = req.headers;
+  //   const { username, password } = req.headers;
 
-  if (username && password) {
-    try {
-      const user = await Auth.getBy({ username });
+  //   if (username && password) {
+  //     try {
+  //       const user = await Auth.getBy({ username });
 
-      if (user && bc.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ error: "You shall not pass!" });
-      }
-    } catch (err) {
-      res
-        .status(400)
-        .json({ error: "User credentials doesn't exist on the database" });
-    }
+  //       if (user && bc.compareSync(password, user.password)) {
+  //         next();
+  //       } else {
+  //         res.status(401).json({ error: "You shall not pass!" });
+  //       }
+  //     } catch (err) {
+  //       res
+  //         .status(400)
+  //         .json({ error: "User credentials doesn't exist on the database" });
+  //     }
+  //   } else {
+  //     res.status(400).json({ error: "Please provide credentials" });
+  //   }
+
+  // with cookies
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(400).json({ error: "Please provide credentials" });
+    res.status(401).json({ error: "You shall not pass" });
   }
 }
 
